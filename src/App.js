0d2578acfb5617a62591a0b8f40a197e37497def
Thanh Nam <hntnam98@gmail.com>
2023-11-15 17:32:37 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello From Docker For React
+
+          <br /> 
+          I m Thanh Nam, i will build this React App on AWS EC2
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
